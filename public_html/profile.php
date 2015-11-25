e88f7f6e5259561055b4fa4424731e0092b66f17
js345 <1994yxf@gmail.com>
2015-11-25 16:13:10 -0600

    Remove W3School

--- public_html/profile.php
+++ public_html/profile.php
@@ -89,16 +89,16 @@ session_start();
 
 <!-- Second Container -->
 <div class="container-fluid bg-2 text-center">
-    <h3 class="margin">Where To Find Me?</h3><br>
+    <h3 class="margin">Your Favorite Songs</h3><br>
     <div class="row">
         <div class="col-sm-4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p></p>
         </div>
         <div class="col-sm-4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p></p>
         </div>
         <div class="col-sm-4">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p></p>
             <!-- img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image" -->
         </div>
     </div>
@@ -107,8 +107,8 @@ session_start();
 
 <!-- Third Container (Grid) -->
 <div class="container-fluid bg-3 text-center">
-    <h3 class="margin">What Am I?</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <h3 class="margin">Find More Songs?</h3>
+    <p>Click the button to find more songs. </p>
     <a href="#" class="btn btn-default btn-lg">
         <span class="glyphicon glyphicon-search"></span> Search
     </a>
@@ -116,7 +116,7 @@ session_start();
 
 <!-- Footer -->
 <footer class="container-fluid bg-4 text-center">
-    <p>Bootstrap Theme Made By <a href="http://www.w3schools.com">www.w3schools.com</a></p>
+    <p><a href="index.html">RadioHub</a></p>
 </footer>
 
 </body>
